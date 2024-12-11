fc0734bf1945110a1d9806ab08c9acca41ee7893
Orsouene <orsoueneelaouizeb@gmail.com>
2024-12-11 19:56:03 +0100

    Aggiunto il css e fatto il bonus

--- src/App.jsx
+++ src/App.jsx
@@ -2,33 +2,47 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+// import "bootstrap/dist/css/bootstrap.min.css";
 import tasks from "./data/menu";
 // importo taskinprogress
 function App() {
-  function tasksInprogress() {
+  function tasksInprogresseBacklog() {
+    // un array per le tasks in progress
     let tasksInprogress = tasks
-      .filter((element) => element.state !== "completed")
+      .filter((element) => element.state === "in_progress")
       .map((element) => (
         <li key={element.id}>
           <p>
-            <strong>{element.title} </strong>
+            <strong>
+              {element.title} <span> {element.state} </span>
+            </strong>
           </p>
-          <p>
-            <strong>Priority : </strong>
-            {element.priority}
-          </p>
-          <p>
-            <strong>Est.time : </strong>
-            {element.estimatedTime}
-          </p>
+          <p>Priority :{element.priority}</p>
+          <p>Est.time :{element.estimatedTime}</p>
         </li>
       ));
+    // un altro array per le tasks in backlog
+    let tasksBacklog = tasks
+      .filter((element) => element.state === "backlog")
+      .map((element) => (
+        <li key={element.id}>
+          <p>
+            <strong>
+              {element.title} <span className="yellow"> {element.state} </span>
+            </strong>
+          </p>
+          <p>Priority :{element.priority}</p>
+          <p>Est.time :{element.estimatedTime}</p>
+        </li>
+      ));
+    // combino le due array
+    const ledueTasks = [...tasksInprogress, ...tasksBacklog];
 
-    return tasksInprogress;
+    return ledueTasks;
   }
-  // Prendo la lungezza del array degli tasks in progress
-  function tasksInprogressLength() {
-    return tasksInprogress().length;
+  // Prendo la lungezza del array degli tasks in progress & in backlog
+  function tasksInprogresseBacklogLength() {
+    return tasksInprogresseBacklog().length;
   }
   // prendo la lunguezza degli tasks completed
   function taskscompletedLength() {
@@ -41,30 +55,33 @@ function App() {
       .map((element) => (
         <li key={element.id}>
           <p>
-            <strong>{element.title} </strong>
+            <strong>
+              {element.title}
+              <span className="green"> {element.state} </span>
+            </strong>
           </p>
-          <p>
-            <strong>Priority : </strong>
-            {element.priority}
-          </p>
-          <p>
-            <strong>Est.time : </strong>
-            {element.estimatedTime}
-          </p>
+          <p>Priority :{element.priority}</p>
+          <p>Est.time :{element.estimatedTime}</p>
         </li>
       ));
 
     return tasksCompleted;
   }
+
   return (
     <>
-      {/* tasks in progress */}
-      <h1>Task manager</h1>
-      <h3> completed tasks({tasksInprogressLength()}) </h3>
-      <ul> {tasksInprogress()} </ul>
-      {/* tasks completed */}
-      <h3> Current tasks({taskscompletedLength()}) </h3>
-      <ul> {tasksCompleted()} </ul>
+      <header>
+        <h1>Task manager</h1>
+      </header>
+      <main>
+        {/* tasks in progress */}
+        <h3>Completed tasks ({tasksInprogresseBacklogLength()}) </h3>
+        <ul> {tasksInprogresseBacklog()} </ul>
+        <hr />
+        {/* tasks completed */}
+        <h3> Current tasks ({taskscompletedLength()}) </h3>
+        <ul>{tasksCompleted()}</ul>
+      </main>
     </>
   );
 }
